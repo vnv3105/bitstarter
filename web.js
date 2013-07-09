ea1d30357a87e12ef904cd1c1cee87569e31fa2a
John Smith <example@stanford.edu>
2013-07-09 22:47:45 +0000

    First try

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-buf = new Buffer(256);
+buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.redFileSync('index.html')));
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
